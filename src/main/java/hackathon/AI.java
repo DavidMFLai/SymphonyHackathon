c35a5e6a37131595a2447636a1a9447b0ab0e213
mflai <davidmf.lai@outlook.com>
2018-06-04 00:47:54 +0800

    added some rule-based processing

--- src/main/java/hackathon/AI.java
+++ src/main/java/hackathon/AI.java
@@ -1,128 +1,127 @@
 package hackathon;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class AI {
+	private int consecutiveRejectCount = 0;
+	private int consecutiveCancelCount = 0;
+	private int consecutiveExecCount = 0;
+
 	public JSONObject processInputEvent(JSONObject jsonObject) {
-		
-		JSONObject reply = null;
-		
-		
-		
-		
-		String issue_type = (String)jsonObject.getString("issue type");
-		if(issue_type.equals("ExecutionEvent")) {
-			reply = handleExecutionEvent(jsonObject);
-		}
-		else if(issue_type.equals("FailoverEvent")) {
-			reply = handleFailoverEvent(jsonObject);
-		}
-		else if(issue_type.equals("RhinosSelfTest")) {
-			reply = handleRhinosSelfTest(jsonObject);
-		}
-		else if(issue_type.equals("NetworkDown")) {
-			reply = handleNetworkDown(jsonObject);
-		}
-		else if(issue_type.equals("PassThroughNetworkDisconnection")) {
-			reply = handlePassThroughNetworkDisconnection(jsonObject);
-		}
-		else {
-			//default case == passthrough:
+		JSONObject reply;
+
+		String issue_type = (String) jsonObject.getString("issue type");
+		System.out.println("Received event[" + issue_type + "]");
+		if (issue_type.equals("Reject")) {
+			reply = handleReject(jsonObject);
+		} else if (issue_type.equals("Cancel")) {
+			reply = handleCancel(jsonObject);
+		} else if (issue_type.equals("Exec")) {
+			reply = handleExec(jsonObject);
+		} else {
+			// default case == passthrough:
 			reply = handlePassthrough(jsonObject);
 		}
-
+		
 		return reply;
 	}
-//	static class JsonKeys
-//	{
-//		static String issueTypeKey 		 	= new String("issue type"); 
-//		static String impactedSystemsKey 	= new String("impacted systems");
-//		static String hostnameKey 		 	= new String("hostname");
-//		static String impactedMarketsKey 	= new String("impacted markets");
-//		static String impactedFlowsKey 		= new String("impacted flows");
-//		static String impactedClientsKey 	= new String("impacted clients");
-//		static String originKey 			= new String("origin");
-//		static String flowTypeKey 			= new String("flow type");
-//		static String pnlKey				= new String("pnl");
-//		static String timestampKey			= new String("timestamp");
-//	}
+
 	private JSONObject handlePassthrough(JSONObject jsonObject) {
 		JSONObject reply = new JSONObject();
 		try {
 			reply.put("issue type", jsonObject.getString("issue type"));
 			reply.put("impacted systems", Arrays.asList(jsonObject.getString("impacted systems").split(",")));
-			reply.put("hostname",  jsonObject.getString("hostname"));
-			reply.put("impacted markets",  Arrays.asList(jsonObject.getString("impacted markets").split(",")));
-			reply.put("impacted flows",  Arrays.asList(jsonObject.getString("impacted flows").split(",")));
+			reply.put("hostname", jsonObject.getString("hostname"));
+			reply.put("impacted markets", Arrays.asList(jsonObject.getString("impacted markets").split(",")));
+			reply.put("impacted flows", Arrays.asList(jsonObject.getString("impacted flows").split(",")));
 			reply.put("impacted clients", Arrays.asList(jsonObject.getString("impacted clients").split(",")));
 			reply.put("pnl", Integer.parseInt(jsonObject.getString("pnl")));
 			reply.put("timestamp", new Date().toString());
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			System.out.println("Unknown exception while processing a reply");
 		}
 		return reply;
 	}
-	
-	private JSONObject handleNetworkDown(JSONObject jsonObject) {
-		JSONObject reply = new JSONObject();
-		try {
-			reply.put("issue type", jsonObject.getString("issue type"));
-			reply.put("impacted systems", Arrays.asList(jsonObject.getString("impacted systems").split(",")));
-			reply.put("hostname",  jsonObject.getString("hostname"));
-			reply.put("impacted markets",  Arrays.asList(jsonObject.getString("impacted markets").split(",")));
-			reply.put("impacted flows",  Arrays.asList(jsonObject.getString("impacted flows").split(",")));
-			reply.put("impacted clients", Arrays.asList(jsonObject.getString("impacted clients").split(",")));
-			reply.put("pnl", Integer.parseInt(jsonObject.getString("pnl")));
-			reply.put("timestamp", new Date().toString());
+
+	private JSONObject handleExec(JSONObject jsonObject) {
+		consecutiveRejectCount = 0;
+		consecutiveCancelCount = 0;
+		consecutiveExecCount++;
+		if (consecutiveExecCount >= 500) {
+			try {
+				JSONObject reply = new JSONObject();
+				reply.put("issue type", "HighVolume");
+				reply.put("impacted systems", Arrays.asList(jsonObject.getString("impacted systems").split(",")));
+				reply.put("hostname", jsonObject.getString("hostname"));
+				reply.put("impacted markets", Arrays.asList(jsonObject.getString("impacted markets").split(",")));
+				reply.put("impacted flows", Arrays.asList(jsonObject.getString("impacted flows").split(",")));
+				reply.put("impacted clients", Arrays.asList(jsonObject.getString("impacted clients").split(",")));
+				reply.put("pnl", Integer.parseInt(jsonObject.getString("pnl")));
+				reply.put("timestamp", new Date().toString());
+				consecutiveExecCount = 0;
+				return reply;
+			} catch (Exception ex) {
+				System.out.println("Unknown exception while processing a reply in handleExec()");
+				return null;
+			}
+		} else {
+			return null;
 		}
-		catch (Exception ex) {
-			System.out.println("Unknown exception while processing a reply");
+	}
+
+	private JSONObject handleReject(JSONObject jsonObject) {
+		consecutiveRejectCount++;
+		consecutiveCancelCount = 0;
+		consecutiveExecCount = 0;
+		if (consecutiveRejectCount >= 500) {
+			try {
+				JSONObject reply = new JSONObject();
+				reply.put("issue type", "RepeatedRejects");
+				reply.put("impacted systems", Arrays.asList(jsonObject.getString("impacted systems").split(",")));
+				reply.put("hostname", jsonObject.getString("hostname"));
+				reply.put("impacted markets", Arrays.asList(jsonObject.getString("impacted markets").split(",")));
+				reply.put("impacted flows", Arrays.asList(jsonObject.getString("impacted flows").split(",")));
+				reply.put("impacted clients", Arrays.asList(jsonObject.getString("impacted clients").split(",")));
+				reply.put("pnl", Integer.parseInt(jsonObject.getString("pnl")));
+				reply.put("timestamp", new Date().toString());
+				consecutiveRejectCount = 0;
+				return reply;
+			} catch (Exception ex) {
+				System.out.println("Unknown exception while processing a reply in handleReject()");
+				return null;
+			}
+		} else {
+			return null;
 		}
-		return reply;
 	}
-	
-	private JSONObject handlePassThroughNetworkDisconnection(JSONObject jsonObject) {
-		JSONObject reply = new JSONObject();
-		reply.put("issue type", "NetworkDisconnection");
-		reply.put("impacted systems", new ArrayList<>(Arrays.asList("OMS", "AlgoEngine")));
-		reply.put("hostname", "bnpserver001");
-		reply.put("impacted markets",  new ArrayList<>(Arrays.asList("HK", "JP")));
-		JSONObject impactedFlows = new JSONObject();
-//		impactedFlows.put("origin", "institutional clients");
-//		impactedFlows.put("flow type", "FnO");
-//		reply.put("impacted flows",  impactedFlows);
-		reply.put("impacted flows",  impactedFlows);
-		reply.put("impacted clients", new ArrayList<>(Arrays.asList("jasmine", "aladdin")));
-		reply.put("pnl", 10000);
-		reply.put("timestamp", new Date().toString());
-		return reply;
+
+	private JSONObject handleCancel(JSONObject jsonObject) {
+		consecutiveRejectCount = 0;
+		consecutiveCancelCount++;
+		consecutiveExecCount = 0;
+		if (consecutiveCancelCount >= 500) {
+			try {
+				JSONObject reply = new JSONObject();
+				reply.put("issue type", "RepeatedCancels");
+				reply.put("impacted systems", Arrays.asList(jsonObject.getString("impacted systems").split(",")));
+				reply.put("hostname", jsonObject.getString("hostname"));
+				reply.put("impacted markets", Arrays.asList(jsonObject.getString("impacted markets").split(",")));
+				reply.put("impacted flows", Arrays.asList(jsonObject.getString("impacted flows").split(",")));
+				reply.put("impacted clients", Arrays.asList(jsonObject.getString("impacted clients").split(",")));
+				reply.put("pnl", Integer.parseInt(jsonObject.getString("pnl")));
+				reply.put("timestamp", new Date().toString());
+				consecutiveCancelCount = 0;
+				return reply;
+			} catch (Exception ex) {
+				System.out.println("Unknown exception while processing a reply in handleReject()");
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
-	
-	
-	
-	private JSONObject handleExecutionEvent(JSONObject jsonObject) {
-		JSONObject reply = new JSONObject();
-		//TODO: processing logic
-		return reply;
-	}
-	
-	private JSONObject handleFailoverEvent(JSONObject jsonObject) {
-		JSONObject reply = new JSONObject();
-		//TODO: processing logic
-		return reply;
-	}
-	
-	private JSONObject handleRhinosSelfTest(JSONObject jsonObject) {
-		JSONObject reply = new JSONObject();
-		reply.put("RhionsTest", "JSON_FROM_AI_MODULE");
-		return reply;
-	}
-	
+
 }
